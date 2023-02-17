71d795dbebf4e947d42f02f95ea08a769d93e73c
ricky-rosay <111669764+ricky-rosay@users.noreply.github.com>
2023-02-17 15:54:24 -0500

    starting server

--- server/server.js
+++ server/server.js
@@ -18,6 +18,19 @@ const { authMiddleware } = require("./utils/auth");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -26,7 +39,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
